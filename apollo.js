b076eab1295f7df22bcc853de11385125ab9b2a5
uttamukkoji <uttamukkoji@gmail.com>
2019-02-14 18:03:51 +0530

    Update apollo.js

--- apollo.js
+++ apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-const GRAPHQL_ENDPOINT = `https://dev-graphql.contentstack.io/stacks/***REMOVED***/explore?access_token=***REMOVED***&environment=production`;
+const GRAPHQL_ENDPOINT = `https://dev-graphql.contentstack.io/stacks/***REMOVED***?access_token=***REMOVED***&environment=production`;
 
 const apolloClient = () => {
   const link = new HttpLink({
